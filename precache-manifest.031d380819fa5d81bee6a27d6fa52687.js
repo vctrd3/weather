f5e48ef7a4ffe2335b05aaf697e393a06d728796
Victor <vctrd3@gmail.com>
2019-05-22 15:25:52 -0700

    Updates

---
+++ precache-manifest.031d380819fa5d81bee6a27d6fa52687.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8709c9ce6e01081cceb8d5bf958aa9",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "c46c399c1f5c2cf09a24",
+    "url": "/weather/static/css/main.3410f314.chunk.css"
+  },
+  {
+    "revision": "083d99d1ed796f8635d5",
+    "url": "/weather/static/js/2.b62f35df.chunk.js"
+  },
+  {
+    "revision": "c46c399c1f5c2cf09a24",
+    "url": "/weather/static/js/main.8ec90e39.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  }
+]);
